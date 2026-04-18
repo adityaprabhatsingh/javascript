2ac5dcff4eecda041d609acd58857b628cd3ddd3
aditya prabhat <adityaprabhat63@email.com>
2026-04-18 20:53:13 +0530

    control flow

---
+++ code/controlflow.js
@@ -0,0 +1,93 @@
+   const temperature =42
+
+   if(temperature<50){
+    console.log("temerpatue us less then 50 ")
+   } else{
+    console.log("temperature is greature than 50 ")
+   }
+
+   console.log("executed")
+
+
+   if(2=="2"){
+    console.log("executer")
+   }
+   if(2==="2"){
+    console.log("executed")
+   }
+
+   // < ,>, <=,>=,/=,==,===
+
+   const score=200;
+
+   if(score<100){
+    console.log("score is less then 100")
+
+
+   }
+  //var we not use because it has global scope it can acess outside the block 
+
+//   const day="tueff"
+//   switch(day){
+//     case "mon":
+//         console.log("monday")
+//         break;
+//     case "tues":
+//         console.log("tuesday")
+//         break;
+
+//     case "wed":
+//         console.log("wednesday")
+//         break;
+//     default:
+//         console.log("default anser")
+//         break;
+
+//   }
+
+//break use to break if not it run all case after correct case but no default case
+
+//falsy value are false,0,-0,BigInt,0n,"",null,undefined,NaN
+
+//truthy valu are "0",'false,"false",[],{}," ",function(){},
+
+
+//check of array 
+
+const userEmail=[]
+
+if(userEmail.length===0){
+    console.log("user email is empty")
+
+}
+
+//how to check a object is empty
+
+const user={}
+
+if(Object.keys(user).length===0){
+    console.log("user object is empty ")
+}
+
+
+//false ==0 ,=='',=="" ->true
+
+//logical operator
+// && ,||
+
+//nullish coalesing operator (??) -> null or undefined.  learn more
+
+let var1;
+
+//var1=5??10
+//var1=null?? 10
+//var1=undefined ?? 15
+//var1=null ?? 10 ?? 15
+
+//console.log(var1)
+
+
+
+// terninary operator 
+
+//condition ? true :false 
---
+++ code/scope.js
@@ -0,0 +1,14 @@
+if(true){
+   let a=10
+   const b=45
+   var c=56
+
+}
+console.log(a)
+console.log(b)
+console.log(c)
+
+
+
+
+
